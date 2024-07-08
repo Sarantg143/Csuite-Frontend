fb587cd5472aaf3c95cbaa90afc1dda49a4f63fc
SARAN TG <113160601+Sarantg143@users.noreply.github.com>
2024-07-08 11:26:41 +0530

    Update Footer.jsx

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,14 +1,11 @@
-import React,{useEffect} from 'react';
+import React, { useEffect } from 'react';
 
 //style.css
 import './Footer.css'
 
 //fontawesome icons 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faLinkedin } from '@fortawesome/free-brands-svg-icons' 
-import { faFacebookF } from '@fortawesome/free-brands-svg-icons' 
-import { faSquareInstagram } from '@fortawesome/free-brands-svg-icons' 
-import { faXTwitter } from '@fortawesome/free-brands-svg-icons' 
+import { faLinkedin, faFacebookF, faSquareInstagram, faXTwitter } from '@fortawesome/free-brands-svg-icons' 
 
 //image upload
 import footerlogo from '../Header/Asset/brand-footer.png'
@@ -19,44 +16,44 @@ import 'aos/dist/aos.css'
 
 function Footer() {
 
-  //aos fatch 
-  useEffect(()=>{
+  //aos fetch 
+  useEffect(() => {
     Aos.init();
-  },[])
+  }, [])
 
   return (
     <div className='footer' id='footer'>
       <section data-aos="fade-up" className='section1'>
-        <img src={footerlogo} alt="" width={"120px"}/>
+        <img src={footerlogo} alt="Footer Logo" width={"120px"} />
         <p>Practical-based learning platform to enhance skills</p>
         <h6>Follow Us on</h6>
         <div className='sociallinks'>
-        <FontAwesomeIcon icon={faLinkedin} />
-        <FontAwesomeIcon icon={faFacebookF} />
-        <FontAwesomeIcon icon={faSquareInstagram} />
-        <FontAwesomeIcon icon={faXTwitter} />
+          <FontAwesomeIcon icon={faLinkedin} />
+          <FontAwesomeIcon icon={faFacebookF} />
+          <FontAwesomeIcon icon={faSquareInstagram} />
+          <FontAwesomeIcon icon={faXTwitter} />
         </div>
       </section>
       <section data-aos="fade-up" className='section2'>
-        <h5 >About LMS</h5>
-        <a>Pricing</a>
-        <a>Features</a>
-        <a>Integrations</a>
-        <a>Events</a>
-        <a>Reviews</a>
+        <h5>About LMS</h5>
+        <a href="#pricing">Pricing</a>
+        <a href="#features">Features</a>
+        <a href="#integrations">Integrations</a>
+        <a href="#events">Events</a>
+        <a href="#reviews">Reviews</a>
       </section>
       <section data-aos="fade-up" className='section3'>
-      <h5 >Support</h5>
-        <a>Contact Us</a>
-        <a>Help Desk</a>
-        <a>Customer Support</a>
-        <a>Professional Services</a>
+        <h5>Support</h5>
+        <a href="#contact">Contact Us</a>
+        <a href="#helpdesk">Help Desk</a>
+        <a href="#support">Customer Support</a>
+        <a href="#services">Professional Services</a>
       </section>
       <section data-aos="fade-up" className='section4'>
-        <h4 >Become an Instructor</h4>
+        <h4>Become an Instructor</h4>
         <p>We only work with the best companies around the globe</p>
         <form className='instructoremail'>
-          <input className="emailinput" type="email" placeholder='Enter Your Email'/>
+          <input className="emailinput" type="email" placeholder='Enter Your Email' />
           <input className='registernow' type="submit" value="Register Now" />
         </form>
       </section>
